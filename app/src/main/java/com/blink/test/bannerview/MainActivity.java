8d49ee1a6d77093dba97d0c22b2a23e605507c14
byblinkdagger <cold_moqiuli@163.com>
2017-03-21 09:30:03 +0800

    fix

--- app/src/main/java/com/blink/test/bannerview/MainActivity.java
+++ app/src/main/java/com/blink/test/bannerview/MainActivity.java
@@ -3,6 +3,7 @@ package com.blink.test.bannerview;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ImageView;
 
 import java.util.ArrayList;
@@ -21,12 +22,16 @@ public class MainActivity extends AppCompatActivity {
         viewList = new ArrayList<View>();
         for (int i = 0; i < imgs.length; i++) {
             ImageView image = new ImageView(this);
+            image.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
+            //设置显示格式
+            image.setScaleType(ImageView.ScaleType.CENTER_CROP);
             image.setImageResource(imgs[i]);
             viewList.add(image);
         }
         bannerView = (BannerView) findViewById(R.id.banner);
         bannerView.setViewList(viewList);
         bannerView.startLoop(true);
+//        bannerView.setTransformAnim(true);
 
     }
 }
